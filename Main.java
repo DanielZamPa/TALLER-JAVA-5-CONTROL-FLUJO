91941d124b84d03154ddbf4165e91f5aca5c8ade
Daniel Zamora Paez <[email>
2025-08-13 15:02:19 -0500

    Creacion de archivos

---
+++ Main.java
@@ -0,0 +1,9 @@
+package Clase3;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner scanMain =new Scanner(System.in);
+
+        scanMain.close();
+    }//Cierre metodo main
+}//Cierre clase main
---
+++ Taller5.java
@@ -0,0 +1,8 @@
+package Clase3;
+
+public class Taller5 {
+    public static void FacturacionMercado(){}
+    public static void ClasificacionMascotas(){}
+    public static void AccesoParqueadero(){}
+    public static void PromocionesRopa(){}
+}
